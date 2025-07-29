fd38eeea0e2d275eae064c081181d241d8dc6cf7
sajid-islam <sajidislam941@gmail.com>
2025-07-29 12:30:13 +0600

    Fix: not finding verifyJwt

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ async function run() {
             });
         });
 
-        app.post("/payments", verifyJWT, async (req, res) => {
+        app.post("/payments", verifyToken, async (req, res) => {
             const payment = req.body;
             const insertResult = await paymentCollection.insertOne(payment);
 
